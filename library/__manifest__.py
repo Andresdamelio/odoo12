2dc3cf1655ff9bd9e6bbaa93e5424d6b9507c196
Andres Damelio <andresdamelio24@gmail.com>
2019-05-06 09:36:54 -0400

    Creando un modulo desde cero, configuracion de manifest.py y creacion de un modelo persistente

---
+++ library/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Managment",
+
+    'summary': """
+        Library Managment
+     """,
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Andres D Amelio",
+    'website': "http://www.andresdamelio.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'installable' : True,
+    # only loaded in demostration mode
+
+}
