1b8d73a8e3f70c991f468e2d4da008130fc8150f
Andres Damelio <andresdamelio24@gmail.com>
2019-05-07 17:00:24 -0400

    Constrains

--- library/models/library_book.py
+++ library/models/library_book.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import api, models, fields, exceptions
 
 
 class LibraryBook(models.Model):
     _name = "library.book"
 
     name = fields.Char(string="Book", default="New")
+    isbn = fields.Char(string="ISBN")
     description = fields.Text(string="Description")
 
     """"
@@ -30,4 +31,16 @@ class LibraryBook(models.Model):
         inverse_name= "book_id",
         string="category"
     )
+
+    # Como definir un constrain para postgres
+    _sql_constraints = [
+        ('name_uniq', 'UNIQUE (name)', 'El nombre del libro es unico, y ya existe en los registros'),
+    ]
+
+    # Como definir un constrain con python
+    @api.constrains()
+    def check_isbn(self):
+        isbn = self.search([['id', '=', self.id]]).mapped("isbn")
+        if self.isbn and self.isbn in isbn:
+            raise exceptions.ValidationError("Isbn duplicado")
 
