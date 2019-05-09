f0dce3085f9dff67e5000108dc1acfb8173c5a12
Andres Damelio <andresdamelio24@gmail.com>
2019-05-09 16:55:58 -0400

    modificando producto

---
+++ product_extend/models/product_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    tipology = fields.Selection(
+        string="Tipo de material",
+        selection=[('metal', 'Metal'), ('pvc', 'PVC'), ('cristal', 'Cristal')]
+    )
